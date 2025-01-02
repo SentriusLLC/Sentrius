a2efbbdd2cc884c59cf1b252cea2034484c3ec7c
Marc Parisi <phrocker@apache.org>
2025-01-02 06:50:52 -0500

    Intermediate commit. In support of Issue #9, adding some basic plumbing around gcp deployments

---
+++ ops-scripts/gcp/base.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+NAMESPACE=sentrius
+CLUSTER=sentrius-autopilot-cluster-1
+REGION=us-east1
---
+++ ops-scripts/gcp/depoloy-helm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+
+source ${SCRIPT_DIR}/base.sh
+source ${SCRIPT_DIR}/../../.gcp.env
+
+helm upgrade --install sentrius ./sentrius-gcp-chart --namespace ${NAMESPACE} \
+    --set sentrius.image.repository=us-central1-docker.pkg.dev/sentrius-project/sentrius-repo/sentrius \
+    --set sentrius.image.tag=${SENTRIUS_VERSION} \
+    --set ssh.image.repository=us-central1-docker.pkg.dev/sentrius-project/sentrius-repo/sentrius-ssh \
+    --set ssh.image.tag=${SENTRIUS_SSH_VERSION} \
+    --set keycloak.image.repository=us-central1-docker.pkg.dev/sentrius-project/sentrius-repo/sentrius-keycloak \
+    --set keycloak.image.tag=${SENTRIUS_KEYCLOAK_VERSION} \
+    --set sentriusagent.image.repository=us-central1-docker.pkg.dev/sentrius-project/sentrius-repo/sentrius-agent \
+    --set sentriusagent.image.tag=${SENTRIUS_AGENT_VERSION} || { echo "Failed to deploy Sentrius with Helm"; exit 1; }
